ad9d89ee8958b69635724111b03d5e9a48f61931
Jason111nn <jason5j288@gmail.com>
2024-12-02 15:58:32 +0800

    J17 web updata

---
+++ api/db.php
@@ -0,0 +1,4 @@
+<?php
+
+$dsn = "mysql:host=localhost;charset=utf8;dbname=t17-2";
+$conn = new PDO($dsn,"admin","1234");
---
+++ api/login.php
@@ -0,0 +1,4 @@
+<?php
+    include_once "db.php";
+    header("Content-Type: application/json");
+    session_start();
