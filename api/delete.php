a5352893933164883f26b72c893bd55efeee8f0e
Jason111nn <jason5j288@gmail.com>
2024-12-08 14:22:11 +0800

    bus delete

--- api/delete.php
+++ api/delete.php
@@ -3,17 +3,10 @@ include_once "db.php";
 session_start();
 
 
-        switch ($_GET["mode"]) {
+        switch ($_POST["mode"]) {
             case 'deletebus':
-                $stmt = $conn->query("DELETE FROM `bus` WHERE $_GET['mode']");
-                break;
-        }
-        switch ($_GET["mode"]) {
-            case 'deletebus':
-                    $id = intval($_GET["id"]); 
-                    $stmt = $conn->prepare("DELETE FROM `bus` WHERE id = :id");
-                    $stmt->bindParam(':id', $id, PDO::PARAM_INT);
-                    echo json_encode(["status" => "success", "message" => "車輛已刪除"]);
+                    $id = intval($_POST["id"]); 
+                    $stmt = $conn->exec("DELETE FROM `bus` WHERE id = $id");
                 break;
         }
         
